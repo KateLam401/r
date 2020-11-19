4b37e7b0c211727e4457bc2e76b2d88e110d9c68
Katherine Lamoureux <katherine.lamoureux@gmail.com>
2020-11-19 09:55:01 -0500

    Update groupBy.r

--- groupBy.r
+++ groupBy.r
@@ -1,32 +1,25 @@
 
-setwd("/home/rstudio/projects/")
+setwd("path")
 
 packages <- c("odbc","dplyr","readr","shinyjs","shiny","shinyWidgets")
 lapply(packages, require, character.only = TRUE)
 
-Micro <- read_csv("/home/rstudio/projects/extract", progress = show_progress(),
+M <- read_csv("path", progress = show_progress(),
 		trim_ws = TRUE, na = c("","NA"), col_types = cols(.default = co_character()))
 
+m <- data.frame(build=c('a','a','a','a','a','a','a'),
+			r=c(1,1,2,2,1,1,2,2),
+			c=c(1,2,1,2,1,2,1,2),
+			f=c(1,2,1,2,1,2,1,2),
+			m=c(50,50,50,50,50,50,50,50))
 
-# for every room, consider all class, for every class consider every freq
-# for every senario, create a df
-# generate a box for every df
-
-
-micro <- data.frame(build=c('a','a','a','a','a','a','a'),
-			room=c(1,1,2,2,1,1,2,2),
-			class=c(1,2,1,2,1,2,1,2),
-			freq=c(1,2,1,2,1,2,1,2),
-			measure=c(50,50,50,50,50,50,50,50))
-
-boxdf <- micro %>%
-	group_by(build, room, class, freq) %>%
+boxdf <- m %>%
+	group_by(b, r, c, f) %>%
 	summarize()
 
-# set veriables
 for (i in 1:nrow(boxdf)){
-	build <- as.character(boxdf[i,"build"])
-	room <- as.character(boxdf[i,"room"])
-	class <- as.character(boxdf[i,"class"])
-	freq <- as.character(boxdf[i,"freq"])
+	b <- as.character(boxdf[i,"build"])
+	r <- as.character(boxdf[i,"room"])
+	c <- as.character(boxdf[i,"class"])
+	f <- as.character(boxdf[i,"freq"])
 }
